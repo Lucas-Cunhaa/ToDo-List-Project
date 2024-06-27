2c76502316f92f11f69603312554ff83c8a827c2
Lucas Cunha <lucasgalvao260@gmail.com>
2024-06-27 13:30:37 -0300

    Adding the stlye of the side bar list

--- my-vue-app/src/components/Lists.tsx
+++ my-vue-app/src/components/Lists.tsx
@@ -1,5 +1,7 @@
+import "../Css/list.css";
+
 interface List {
-  id: string;
+  id: number;
   title: string;
   description: string;
 }
@@ -7,28 +9,51 @@ interface List {
 const Lists = () => {
   const lists: List[] = [
     {
-      id: "1",
+      id: 1,
       title: "Law course",
       description: "Law course homework's",
     },
     {
-      id: "2",
+      id: 2,
       title: "Mathematic",
       description: "Mathematic homework's",
     },
     {
-      id: "3",
+      id: 3,
       title: "Personal Project",
       description: "Personal Project tasks to do",
     },
+    {
+      id: 4,
+      title: "Python",
+      description: "Mr's Dalton tasks",
+    },
   ];
 
+  const isPar = (id: number) => {
+    if (id % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
-    <> 
+    <>
       {lists.map(({ id, title, description }: List) => (
-        <div className="list-section" key={id}>
-          <h2 className="list-name">{title}</h2>
-          <h3 className="list-description">{description}</h3>
+        <div key={id}>
+          {isPar(id) && <div className="list-section background-gray">
+            <div className="list-section" key={id}>
+            <h2 className="list-name">{title}</h2>
+            <h3 className="list-description">{description}</h3>
+          </div>
+           </div>
+           }
+           {!isPar(id) && <div className="list-section" key={id}>
+            <h2 className="list-name">{title}</h2>
+            <h3 className="list-description">{description}</h3>
+          </div> 
+          }
+         
         </div>
       ))}
     </>
